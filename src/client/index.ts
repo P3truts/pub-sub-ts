f5585edd82085625a8565f5c58d4695b7e6c65b2
P3truts <obreja.petrut@gmail.com>
2026-06-10 20:33:08 +0300

    B: add client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,38 @@
+import amqp from "amqplib";
+import process from "node:process";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { SimpleQueueType } from "../internal/pubsub/publish.js";
+// import { publishJSON } from "../internal/pubsub/publish.js";
+// import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+// import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+
+
 async function main() {
   console.log("Starting Peril client...");
+  const connString = "amqp://guest:guest@localhost:5672/";
+
+  try {
+    const conn = await amqp.connect(connString);
+    //const channel = await conn.createConfirmChannel();
+    // const playState: PlayingState = { isPaused: true };
+    // const value = JSON.stringify(playState);
+    // publishJSON(channel, ExchangePerilDirect, PauseKey, value);
+    if (conn) {
+      console.log("Connection successful!");
+    }
+
+    const username = await clientWelcome();
+    let [channel, queue] = await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient);
+
+    process.on('exit', (code) => {
+      console.log('Process exit event with code: ', code);
+      conn.close();
+    });
+  } catch (err) {
+    console.log("Connection failed: ", err);
+  }
 }
 
 main().catch((err) => {
